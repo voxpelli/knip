9c1e23cb4b31ab87399a3521a5f245099b730312
Lars Kappert <lars@webpro.nl>
2024-03-10 10:23:07 +0100

    Migrate plugin tests

--- packages/knip/test/plugins/cspell.test.ts
+++ packages/knip/test/plugins/cspell.test.ts
@@ -1,12 +1,25 @@
 import assert from 'node:assert/strict';
 import test from 'node:test';
-import { default as cspell } from '../../src/plugins/cspell/index.js';
-import { resolve, join } from '../../src/util/path.js';
+import { main } from '../../src/index.js';
+import { resolve } from '../../src/util/path.js';
+import baseArguments from '../helpers/baseArguments.js';
+import baseCounters from '../helpers/baseCounters.js';
 
 const cwd = resolve('fixtures/plugins/cspell');
 
-test('Find dependencies in cspell configuration (json)', async () => {
-  const configFilePath = join(cwd, '.cspell.json');
-  const dependencies = await cspell.findDependencies(configFilePath, {});
-  assert.deepEqual(dependencies, ['@cspell/dict-cryptocurrencies/cspell-ext.json']);
+test('Find dependencies with the Cspell plugin', async () => {
+  const { issues, counters } = await main({
+    ...baseArguments,
+    cwd,
+  });
+
+  assert(issues.unlisted['.cspell.json']['@cspell/dict-cryptocurrencies/cspell-ext.json']);
+
+  assert.deepEqual(counters, {
+    ...baseCounters,
+    devDependencies: 1,
+    unlisted: 2, // TODO fast-glob returns two files cspell.json and .cSpell.json while there's only one file on disk
+    processed: 0,
+    total: 0,
+  });
 });
--- packages/knip/test/plugins/eleventy3.test.ts
+++ packages/knip/test/plugins/eleventy3.test.ts
@@ -5,21 +5,17 @@ import { resolve } from '../../src/util/path.js';
 import baseArguments from '../helpers/baseArguments.js';
 import baseCounters from '../helpers/baseCounters.js';
 
-const cwd = resolve('fixtures/plugins/angular');
+const cwd = resolve('fixtures/plugins/eleventy3');
 
-test('Find dependencies with the Angular plugin', async () => {
-  const { issues, counters } = await main({
+test('Find dependencies with the Eleventy plugin (3)', async () => {
+  const { counters } = await main({
     ...baseArguments,
     cwd,
   });
 
-  assert(issues.unlisted['angular.json']['@angular-devkit/build-angular']);
-  assert(issues.unlisted['angular.json']['jasmine']);
-
   assert.deepEqual(counters, {
     ...baseCounters,
-    unlisted: 2,
-    processed: 1,
-    total: 1,
+    processed: 5,
+    total: 5,
   });
 });
